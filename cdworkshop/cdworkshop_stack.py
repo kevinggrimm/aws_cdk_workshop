5aafee06528ebccef17571cd072f5c288706025f
Kevin Grimm <kevin.g.grimm1@gmail.com>
2020-06-25 10:40:00 -0400

    cdk example

--- cdworkshop/cdworkshop_stack.py
+++ cdworkshop/cdworkshop_stack.py
@@ -1,23 +1,37 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    core,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
+
+from cdk_dynamo_table_viewer import TableViewer
+from hitcounter import HitCounter
 
 class CdworkshopStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdworkshopQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7, # Runtime environment
+            code=_lambda.Code.from_asset('lambda'), # Loaded from Lambda dir
+            handler='hello.handler', # Name of file, function
         )
 
-        topic = sns.Topic(
-            self, "CdworkshopTopic"
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_counter.handler,
+        )
+
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
+            sort_by='-hits'
+        )
